93fb376b19c1149f4c8ce083a399a5f061098b8a
Greg Trowbridge <gjtrowbridge@gmail.com>
2018-02-09 15:13:03 -0800

    Add openid example code.

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Provider = require('oidc-provider');
+const passport = require('passport');
+const OpenIDStrategy = require('passport-openid').Strategy;
+
+passport.use(new OpenIDStrategy(
+  {
+    returnURL: 'https://whispering-retreat-73590.herokuapp.com/auth/openid/return',
+    realm: 'https://whispering-retreat-73590.herokuapp.com',
+  },
+  (identifier, done) => {
+    console.log('xcxc creating user with id:', identifier);
+    const user = {
+      identifier,
+    };
+    done(undefined, user);
+  }
+));
 
 const port = process.env.PORT || 8050;
 
@@ -10,21 +25,27 @@ const app = express();
 // Add middleware
 app.use(bodyParser.json({ type: 'application/json', limit: '50mb' }));
 
-// Create endpoints
-const router = express.Router();
-app.use('/', router);
+// Serve static files
+app.use(express.static(__dirname + '/public'));
 
-const oidc = new Provider(`http://localhost:${port}`);
-oidc.initialize().then(() => {
-  app.use('/oidc', oidc.callback);
-});
+// Create auth endpoints
+const authRouter = express.Router();
+app.use('/auth', authRouter);
+authRouter.post('/openid', passport.authenticate('openid'));
+authRouter.get('/openid/return', passport.authenticate('openid', {
+  successRedirect: '/success',
+  failureRedirect: '/failure',
+}));
 
-router.get('/', (req, res) => {
+// Create API endpoints
+const apiRouter = express.Router();
+
+app.use('/api', apiRouter);
+apiRouter.get('/', (req, res) => {
   res.status(200).json({
     message: 'hi greg',
   });
 });
-
 
 // Start server
 const server = app.listen(port, function() {
