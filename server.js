dc65ec47318b93d94a47db2552fb14ce95fe05c9
Greg Trowbridge <gjtrowbridge@gmail.com>
2018-02-17 19:57:19 -0800

    Add redirect on login endpoint.

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/', session: true }),
   (req, res) => {
     console.log('wooo we authenticated, here is our user object:', req.user);
-    res.json(req.user);
+    // res.json(req.user);
+    res.redirect('/');
   }
 );
 
